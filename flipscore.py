c7ba07174f533cce82087af80f5d0d75fb546325
Josh Bemis <76860466+joshbemis@users.noreply.github.com>
2025-08-14 12:54:35 -0600

    add win condition function

--- flipscore.py
+++ flipscore.py
@@ -1,6 +1,6 @@
 """
-version 0.1 
-created 2025-08-13
+version 0.6 
+created 2025-08-14
 Scores for Flip 7 are running totals
 default winning condition is a score of 200
 each player that ends the round with cards adds the total of their cards
@@ -11,11 +11,13 @@ guided will allow the user to input the values of each individual card, add thos
 
 maybe allow two differnet table displays: by round and just total by player
 TODO: 
-    v 0.5 tweak winning condition message to state who won and with what score
-    v 0.6 handle winning ties (if wanted. instructions say first player to 200, so home rule if not)
-    v 0.9 error handling
-    v 1.0 clean up and README
-    v 1.1 implement guided mode (probably just an extra function)
+    D - v0.5 tweak winning condition message to state who won and with what score
+    D - v0.6 handle winning ties (if wanted. instructions say first player to 200, so home rule if not)
+    v0.7 added during 0.6 work, will need to reset player order each round to help say dealer
+    v0.9 error handling
+    v0.9.5 clean up variable names, formatting and promp language
+    v1.0 README and release
+    v1.1 implement guided mode (probably just an extra function)
 """
 
 from tabulate import tabulate
@@ -40,16 +42,18 @@ def intro():
     print("This program has two modes: basic and guided. In basic mode, you will need to add each player's cards at the end of each round and enter the total.")
     print("In guided mode, you will enter each card individually and the program will total them for you.\n")
     # program_mode = input("Enter 1 for basic score keeping mode and 2 for guided: ")
-    win_score = int(input(f"You chose basic mode. What score would you like to play to? The default is 200: "))
+    program_mode = "basic"
+    win_score = int(input(f"You chose {program_mode} mode. What score would you like to play to? The default is 200: "))
     print("\n")
     
+    # return program mode as well when implemented
     return win_score
-    # return program_mode and win score
 
 def play_round(players, round_number, cur_totals):
     """Pulls the score for each player from input and sends the round scores and totals back as lists"""
     round_scores = [round_number]
     print(f"~~~~~Starting round number {round_number}!~~~~~")
+    print(f"PLAYER deals this round.")
     for player in players:
         round_scores.append(int(input(f"What was {player}'s score? ")))
     
@@ -57,7 +61,29 @@ def play_round(players, round_number, cur_totals):
     for score in range(1, len(round_scores)):
         cur_totals[score] += round_scores[score]
 
-    return round_scores, cur_totals
+    # call reorder function
+
+    return round_scores, cur_totals # return new player list
+
+def winning_player(players, cur_totals, win_score):
+    """Accepts the player list and the current totals and returns the player with highest score"""
+    winners = []
+    start_value = 1 # since total list contains "Total" as first value we start at the second list value
+    
+    for total in cur_totals[1:]:
+        if total >= win_score:
+            winners.append(players[cur_totals.index(total, start_value) - 1])
+            start_value += 1
+
+    for winner in winners:
+        if winners.index(winner) == 0:
+            print(f"The first person to achieve {win_score} points was {winner}. They are our overall winner!")
+        else:
+            print(f"The next person to achieve {win_score} points was {winner}. Too slow!")
+
+#def reset_order():
+    # function to reset the order for each round
+    # will also need to reset order of scores
 
 if __name__ == "__main__":
     # say hello to the player, give instructions and get the list of players
@@ -82,5 +108,5 @@ if __name__ == "__main__":
             print("\n")
             round_number += 1
         else:
-            print("Winning score!")
+            winning_player(player_list, player_totals, win_score)
             break
---
+++ test.py
@@ -0,0 +1,5 @@
+scores = [15, 20, 50, 3]
+names = ["Round", "Josh", "Marilyn", "Will"]
+
+winning_score = scores.index(max(scores))
+print(names[winning_score])
